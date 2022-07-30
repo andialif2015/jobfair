f9de8670a8b6f8fc2c8138dbbbfc29ab2e5a4c4a
andialif <andialif2015@gmail.com>
2022-07-30 22:09:03 +0800

    change config database

--- config/config.js
+++ config/config.js
@@ -1,9 +1,16 @@
 
-const DB_USERNAME = '';
+
+// const DB_USERNAME = 'lenteral_jobfairadmin';
+// const DB_PASSWORD = 'Cgq4sLWge(~l';
+// const DB_NAME = 'lenteral_jobfair';
+// const DB_HOST = '103.150.60.234';
+// const DB_DIALECT = 'mysql';
+
+const DB_USERNAME = 'root';
 const DB_PASSWORD = '';
-const DB_NAME = '';
-const DB_HOST = '';
-const DB_DIALECT = '';
+const DB_NAME = 'jobfair';
+const DB_HOST = 'localhost';
+const DB_DIALECT = 'mysql';
 
 
 module.exports = {
--- controllers/auth.js
+++ controllers/auth.js
@@ -39,7 +39,7 @@ module.exports = {
 
             const passwordHash = await bcrypt.hash(password, 10);
             
-
+            return res.send(req.body);
             const newUser = await User.create({
                 nama: nama,
                 email: email,
