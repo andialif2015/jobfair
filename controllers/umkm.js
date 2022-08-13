153910827f2d77cb92a063b6fad1020e94ba0f8a
andialif <andialif2015@gmail.com>
2022-08-14 01:00:26 +0800

    cehck add lowongan

--- controllers/umkm.js
+++ controllers/umkm.js
@@ -116,7 +116,7 @@ module.exports = {
             return res.status(200).json({
                 status: true,
                 message: "Berhasil tambahkan lowongan pekerjaan",
-                data: lowongan
+                data: umkm
             })
         }catch(err){
             return res.status(500).json({
